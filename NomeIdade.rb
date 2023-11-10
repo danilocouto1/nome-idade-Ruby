cc2f073e31ee39aeadfe648c47e1eb5d069f7d58
Yehior <dansantosalfa@gmail.com>
2023-11-10 14:59:50 -0300

    Correção de get para gets

--- NomeIdade.rb
+++ NomeIdade.rb
@@ -1,7 +1,7 @@
 puts "Bem Vindo a Biblioteca Municipal de Jequié"
 print "Qual seu nome completo: "
-nome = get.chomp
+nome = gets.chomp
 print "Qual sua idade: "
-idade = get.chomp.to_i
+idade = gets.chomp.to_i
 
 puts "Bem vindo #{nome} que possui #{idade}!"
