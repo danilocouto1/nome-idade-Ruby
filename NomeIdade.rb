6ba8ce1daeee048df919f8879186918ae47019ba
Yehior <dansantosalfa@gmail.com>
2023-11-10 14:54:25 -0300

    Primeiro codigo Ruby

---
+++ NomeIdade.rb
@@ -0,0 +1,7 @@
+puts "Bem Vindo a Biblioteca Municipal de Jequié"
+print "Qual seu nome completo: "
+nome = get.chomp
+print "Qual sua idade: "
+idade = get.chomp.to_i
+
+puts "Bem vindo #{nome} que possui #{idade}!"
